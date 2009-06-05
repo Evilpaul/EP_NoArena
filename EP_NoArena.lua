654027001c79b4e33eb755bece90ecc4fbad253c
Evilpaul <yj589794@googlemail.com>
2009-06-05 23:39:03 +0100

    add toc tags and finalise release

--- EP_NoArena.lua
+++ EP_NoArena.lua
@@ -2,8 +2,7 @@ local EPNoArena = CreateFrame("Frame");
 EPNoArena:RegisterEvent("ARENA_TEAM_INVITE_REQUEST");
 EPNoArena:RegisterEvent("PETITION_SHOW");
 
-local arenaInviteDeclineMessage = "This is an automated message, all arena invitations will be declined";
-local arenaPetitionDeclineMessage = "This is an automated message, all arena petitions will be declined";
+local arenaDeclineMessage = "This is an automated message, all arena invitations will be declined";
 
 function EPNoArena:MessageOutput(inputMessage)
 	ChatFrame1:AddMessage("|cffDAFF8A[No Arena]|r " .. inputMessage);
@@ -30,7 +29,7 @@ function EPNoArena:DeclineArenaInvite()
 	self:MessageOutput(string.format("Declined invitation from %s to join %s", arg1, arg2));
 
 	-- tell the idiot
-	SendChatMessage(arenaInviteDeclineMessage, "WHISPER", nil, arg1);
+	SendChatMessage(arenaDeclineMessage, "WHISPER", nil, arg1);
 end;
 
 function EPNoArena:DeclineArenaPetition(arenaName, originator, isOriginator)
@@ -43,7 +42,7 @@ function EPNoArena:DeclineArenaPetition(arenaName, originator, isOriginator)
 		self:MessageOutput(string.format("Declined arena petition from %s to join %s", originator, arenaName));
 
 		-- tell the idiot
-		SendChatMessage(arenaPetitionDeclineMessage, "WHISPER", nil, arg1);
+		SendChatMessage(arenaDeclineMessage, "WHISPER", nil, arg1);
 	else
 		-- report to user
 		self:MessageOutput(string.format("Declined arena petition to join %s", arenaName));
@@ -53,6 +52,7 @@ end;
 function EPNoArena:FilterPetition()
 	local petitionType, title, _, _, originator, isOriginator, _ = GetPetitionInfo();
 
+	-- we do not care about guild, or other, petitions
 	if petitionType == "arena" then
 		self:DeclineArenaPetition(title, originator, isOriginator);
 	end;
@@ -68,6 +68,5 @@ end);
 
 -- filter out our responses
 ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", function(_, _, msg, player)
-	if msg == arenaInviteDeclineMessage then return true; end; -- filter out the reply whisper
-	if msg == arenaPetitionDeclineMessage then return true; end; -- filter out the reply whisper
+	if msg == arenaDeclineMessage then return true; end; -- filter out the reply whisper
 end);
