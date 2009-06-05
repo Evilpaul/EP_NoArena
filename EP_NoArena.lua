12b59a2b4a3e46cc14111d3fbaf94fb9126f7d5c
Evilpaul <yj589794@googlemail.com>
2009-06-05 19:09:05 +0100

    first commit

---
+++ EP_NoArena.lua
@@ -0,0 +1,73 @@
+local EPNoArena = CreateFrame("Frame");
+EPNoArena:RegisterEvent("ARENA_TEAM_INVITE_REQUEST");
+EPNoArena:RegisterEvent("PETITION_SHOW");
+
+local arenaInviteDeclineMessage = "This is an automated message, all arena invitations will be declined";
+local arenaPetitionDeclineMessage = "This is an automated message, all arena petitions will be declined";
+
+function EPNoArena:MessageOutput(inputMessage)
+	ChatFrame1:AddMessage("|cffDAFF8A[No Arena]|r " .. inputMessage);
+end;
+
+function EPNoArena:DeclineArenaInvite()
+
+	-- automatically decline
+	DeclineArenaTeam();
+
+	-- The popup is shown, make sure it goes away
+	for i = 1, STATICPOPUP_NUMDIALOGS do
+		local popup = _G["StaticPopup" .. i]
+
+		if popup then
+			if popup:IsVisible() and
+			   popup.which == "ARENA_TEAM_INVITE" then
+				popup:Hide();
+			end
+		end
+	end
+
+	-- report to user
+	self:MessageOutput(string.format("Declined invitation from %s to join %s", arg1, arg2));
+
+	-- tell the idiot
+	SendChatMessage(arenaInviteDeclineMessage, "WHISPER", nil, arg1);
+end;
+
+function EPNoArena:DeclineArenaPetition(arenaName, originator, isOriginator)
+
+	-- automatically decline the petition
+	ClosePetition();
+
+	if isOriginator == true then
+		-- report to user
+		self:MessageOutput(string.format("Declined arena petition from %s to join %s", originator, arenaName));
+
+		-- tell the idiot
+		SendChatMessage(arenaPetitionDeclineMessage, "WHISPER", nil, arg1);
+	else
+		-- report to user
+		self:MessageOutput(string.format("Declined arena petition to join %s", arenaName));
+	end;
+end;
+
+function EPNoArena:FilterPetition()
+	local petitionType, title, _, _, originator, isOriginator, _ = GetPetitionInfo();
+
+	if petitionType == "arena" then
+		self:DeclineArenaPetition(title, originator, isOriginator);
+	end;
+end;
+
+EPNoArena:SetScript("OnEvent", function(self, event, addon)
+	if event == "ARENA_TEAM_INVITE_REQUEST" then
+		self:DeclineGuildInvite();
+	elseif event == "PETITION_SHOW" then
+		self:FilterPetition();
+	end;
+end);
+
+-- filter out our responses
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", function(_, _, msg, player)
+	if msg == arenaInviteDeclineMessage then return true; end; -- filter out the reply whisper
+	if msg == arenaPetitionDeclineMessage then return true; end; -- filter out the reply whisper
+end);
