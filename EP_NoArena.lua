5dfa8d74f1f5b57312f4b3b76a14c0ffe8b07734
Evilpaul <yj589794@googlemail.com>
2009-06-08 23:14:04 +0100

    do some tidying up

--- EP_NoArena.lua
+++ EP_NoArena.lua
@@ -1,72 +1,67 @@
-local EPNoArena = CreateFrame("Frame");
-EPNoArena:RegisterEvent("ARENA_TEAM_INVITE_REQUEST");
-EPNoArena:RegisterEvent("PETITION_SHOW");
+local EPNoArena = CreateFrame("Frame")
+EPNoArena:RegisterEvent("ARENA_TEAM_INVITE_REQUEST")
+EPNoArena:RegisterEvent("PETITION_SHOW")
 
-local arenaDeclineMessage = "This is an automated message, all arena invitations will be declined";
+local arenaDeclineMessage = "This is an automated message, all arena invitations will be declined"
 
 function EPNoArena:MessageOutput(inputMessage)
-	ChatFrame1:AddMessage("|cffDAFF8A[No Arena]|r " .. inputMessage);
-end;
+	ChatFrame1:AddMessage("|cffDAFF8A[No Arena]|r " .. inputMessage)
+end
 
-function EPNoArena:DeclineArenaInvite()
+function EPNoArena:ARENA_TEAM_INVITE_REQUEST(self, event, ...)
 
 	-- automatically decline
-	DeclineArenaTeam();
+	DeclineArenaTeam()
 
 	-- The popup is shown, make sure it goes away
 	for i = 1, STATICPOPUP_NUMDIALOGS do
 		local popup = _G["StaticPopup" .. i]
 
-		if popup then
-			if popup:IsVisible() and
-			   popup.which == "ARENA_TEAM_INVITE" then
-				popup:Hide();
-			end
+		if popup and
+		   popup:IsVisible() and
+		   popup.which == "ARENA_TEAM_INVITE" then
+			popup:Hide()
 		end
 	end
 
 	-- report to user
-	self:MessageOutput(string.format("Declined invitation from %s to join %s", arg1, arg2));
+	self:MessageOutput(string.format("Declined invitation from %s to join %s", arg1, arg2))
 
 	-- tell the idiot
-	SendChatMessage(arenaDeclineMessage, "WHISPER", nil, arg1);
-end;
+	SendChatMessage(arenaDeclineMessage, "WHISPER", nil, arg1)
+end
 
 function EPNoArena:DeclineArenaPetition(arenaName, originator, isOriginator)
 
 	-- automatically decline the petition
-	ClosePetition();
+	ClosePetition()
 
 	if isOriginator == true then
 		-- report to user
-		self:MessageOutput(string.format("Declined arena petition from %s to join %s", originator, arenaName));
+		self:MessageOutput(string.format("Declined arena petition from %s to join %s", originator, arenaName))
 
 		-- tell the idiot
-		SendChatMessage(arenaDeclineMessage, "WHISPER", nil, arg1);
+		SendChatMessage(arenaDeclineMessage, "WHISPER", nil, arg1)
 	else
 		-- report to user
-		self:MessageOutput(string.format("Declined arena petition to join %s", arenaName));
-	end;
-end;
+		self:MessageOutput(string.format("Declined arena petition to join %s", arenaName))
+	end
+end
 
-function EPNoArena:FilterPetition()
-	local petitionType, title, _, _, originator, isOriginator, _ = GetPetitionInfo();
+function EPNoArena:PETITION_SHOW(self, event, ...)
+	local petitionType, title, _, _, originator, isOriginator, _ = GetPetitionInfo()
 
 	-- we do not care about guild, or other, petitions
 	if petitionType == "arena" then
-		self:DeclineArenaPetition(title, originator, isOriginator);
-	end;
-end;
+		self:DeclineArenaPetition(title, originator, isOriginator)
+	end
+end
 
-EPNoArena:SetScript("OnEvent", function(self, event, addon)
-	if event == "ARENA_TEAM_INVITE_REQUEST" then
-		self:DeclineGuildInvite();
-	elseif event == "PETITION_SHOW" then
-		self:FilterPetition();
-	end;
-end);
+EPNoArena:SetScript("OnEvent", function(self, event, ...)
+	self[event](self, event, ...)
+end)
 
 -- filter out our responses
 ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", function(_, _, msg, player)
 	if msg == arenaDeclineMessage then return true; end; -- filter out the reply whisper
-end);
+end)
